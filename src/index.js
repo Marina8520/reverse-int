a7a5ec3ea81e0f448095fa6a1942b40f14375ad5
Marina8520 <4079722@mail.ru>
2022-02-06 01:23:19 +0300

    feat: add reverseInt function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let numArray= [],
+    numArrayReverse = [];
+
+    n = Math.abs(n);
+    numArray = Array.from(String(n));
+    for(let i = 0; i < numArray.length; i++) {
+        numArrayReverse.unshift(numArray[i]);
+        console.log(numArray[i]);
+        }
+        return numArrayReverse.join('');
 }
